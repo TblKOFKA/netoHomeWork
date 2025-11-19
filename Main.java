0a4bd358a6ae010f0788ff0e3dea26fb5374a189
TblKOFKA <56540031+TblKOFKA@users.noreply.github.com>
2025-11-19 16:11:27 +0300

    HomeWork 01-01

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Irina Amelchenkova");
+
+        double income = 100000;
+        double spending = 30000.56;
+
+        System.out.println("Итого:");
+        System.out.println(income - spending);
+
+    }
+}
